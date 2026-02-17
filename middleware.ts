1edb4c2467bde27943e1957e57ef81fd0ec311d3
hamza elmaaloumi <hamzaelmaaloumi@gmail.com>
2026-02-17 22:42:21 +0000

    fix footer and navbar color

--- middleware.ts
+++ middleware.ts
@@ -7,9 +7,11 @@ export default withAuth({
 })
 
 export const config = { 
-  // We use only two clean strings here to avoid the "matcher[2]" error
   matcher: [
     "/cards/:path*", 
     "/api/cards/:path*",
+    "/profile/:path*",
+    "/settings/:path*",
+    "/ranking/:path*"
   ] 
 }
